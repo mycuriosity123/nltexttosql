1bd5a9ccaaf357cfd60fec0ce91914b27ba107ca
saikiran <saip89199@gmail.com>
2025-02-20 02:11:19 +0530

    updated req.txt

---
+++ setup.py
@@ -0,0 +1,11 @@
+from setuptools import find_packages, setup
+
+setup(
+    name = 'NLTOSQL AI Project',
+    version= '0.0.0',
+    author= 'kiran',
+    author_email= '',
+    packages= find_packages(),
+    install_requires = []
+
+)
